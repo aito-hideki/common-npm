7f7dcbc24c47d2630c385790c0957ee0059c160f
Aito Hideki <aito.hideki@gmail.com>
2020-10-29 04:49:46 -0400

    moved index to front

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import './plugins/composition-api'
-import './plugins/validators'
-import './plugins/fragment'
-import './components'
-
-import './styles/index.scss'
-export { default as useVuetify } from './plugins/vuetify'
